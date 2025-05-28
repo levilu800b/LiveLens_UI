ed6bbe001a36b74022bffb9f2941a5574d5bc7ec
levilu800b <levilu800b@gmail.com>
2025-05-28 22:54:31 +0100

    Fixed mouse icon mobile responsive issue

--- src/pages/Home/container/Hero.tsx
+++ src/pages/Home/container/Hero.tsx
@@ -134,10 +134,10 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-        <div className="w-6 h-10 border-2 border-white/30 rounded-full flex justify-center">
-          <div className="w-1 h-3 bg-gradient-to-b from-purple-400 to-pink-400 rounded-full mt-2 animate-pulse"></div>
+      {/* Responsive Scroll Indicator */}
+      <div className="absolute bottom-4 sm:bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+        <div className="w-5 h-8 sm:w-6 sm:h-10 border-2 border-white/30 rounded-full flex justify-center">
+          <div className="w-0.5 sm:w-1 h-2 sm:h-3 bg-gradient-to-b from-purple-400 to-pink-400 rounded-full mt-1.5 sm:mt-2 animate-pulse"></div>
         </div>
       </div>
     </section>
